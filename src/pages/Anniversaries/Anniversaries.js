fa070f47fb9148f1fd17191ab8db0b8ad6c6d21b
Calvin <calvinng1993@gmail.com>
2024-01-26 07:31:34 +0000

    Complete Gift Page

--- src/pages/Anniversaries/Anniversaries.js
+++ src/pages/Anniversaries/Anniversaries.js
@@ -1,5 +1,22 @@
 import "./Anniversaries.scss";
+import backArrow from "../../assets/icons/arrow-left.svg";
 const Anniversaries = () => {
-  return <div>Anniversaries</div>;
+  return (
+    <>
+      <header className="gifts">
+        <img src={backArrow} alt="" className="gifts__img" />
+        <h1 className="gifts__title">Anniversaries</h1>
+        <h3 className="gifts__description">
+          Get something special for a special someone
+        </h3>
+      </header>
+      <section className="manage-address">
+        <p className="manage-address__description">Sending to someone else?</p>
+        <button className="manage-address__button">
+          Manage gift addresses
+        </button>
+      </section>
+    </>
+  );
 };
 export default Anniversaries;
--- src/pages/GiftsPage/GiftsPage.js
+++ src/pages/GiftsPage/GiftsPage.js
@@ -1,5 +1,48 @@
 import "./GiftsPage.scss";
+import backArrow from "../../assets/icons/arrow-left.svg";
+import { Link } from "react-router-dom";
+import occasionJSON from "../../data/occasion.json";
+
 const GiftsPage = () => {
-  return <div>GiftsPage</div>;
+  return (
+    <>
+      <header className="gifts">
+        <Link to="/">
+          <img src={backArrow} alt="" className="gifts__img" />
+        </Link>
+        <h1 className="gifts__title">Gifts</h1>
+        <h3 className="gifts__description">
+          Explore gifts to shop now, or send straight to someone’s door
+        </h3>
+      </header>
+      <section className="manage-address">
+        <p className="manage-address__description">Sending to someone else?</p>
+        <button className="manage-address__button">
+          Manage gift addresses
+        </button>
+      </section>
+      <main className="main">
+        <h4 className="main__title">Celebrate, treat or care for a friend!</h4>
+        <section className="occasion">
+          {occasionJSON.map((occasion) => {
+            return (
+              <Link
+                to={`/${occasion.occasion}`}
+                className="occasion__container"
+                key={occasion.id}
+              >
+                <img
+                  src={occasion.image}
+                  alt={occasion.occasion}
+                  className="occasion__img"
+                />
+                <p className="occasion__title">{occasion.occasion}</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
+    </>
+  );
 };
 export default GiftsPage;
