df545441ee5a8300de44ac72ed586d5d36c80ff4
awhr95 <awhr95@gmail.com>
2024-01-26 13:36:55 +0000

    Add styling add address

--- src/component/ManageAddress/ManageAddress.js
+++ src/component/ManageAddress/ManageAddress.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import "./ManageAddress.scss";
 import location from "../../assets/icons/location-marker.svg";
+import currentLocation from "../../assets/icons/location.svg";
+import plus from "../../assets/icons/plus.svg";
 
 function ManageAddress() {
   const [addressList, setAddressList] = useState(null);
@@ -25,15 +27,25 @@ function ManageAddress() {
     return <p>Loading...</p>;
   }
 
+  // function onClick()
+
   return (
     <main className="manage-addresses">
-      <h1 className="manage-addresses__title">Deliver to...</h1>
-      <Link to={"/add-address"} className="manage-addresses__link">
-        <p>New address</p>
-      </Link>
-      <Link to={"#"} className="manage-addresses__link">
-        <p>Current location</p>
-      </Link>
+      <div className="manage-addresses-header">
+        <h1 className="manage-addresses__title">Deliver to...</h1>
+        <Link to={"/add-address"} className="manage-addresses__link">
+          <img className="manage-addresses__current-location" src={plus} />
+          <p className="manage-addresses__link-content">New address</p>
+        </Link>
+        <Link to={"#"} className="manage-addresses__link--current">
+          <img
+            className="manage-addresses__current-location"
+            src={currentLocation}
+          />
+          <p className="manage-addresses__link-content">Current location</p>
+        </Link>
+      </div>
+
       <ul className="manage-addresses__list">
         {addressList.map((address) => {
           return (
