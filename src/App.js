179986e64059ee947596a657ec4185f40f7c1ce7
Calvin <calvinng1993@gmail.com>
2024-01-26 13:09:49 +0000

    Add Gift model to page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GiftsPage from "./pages/GiftsPage/GiftsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import Anniversaries from "./pages/Anniversaries/Anniversaries";
+import GiftRecieveModal from "./component/GiftRecieveModal/GiftRecieveModal";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/giftmodal" element={<GiftRecieveModal />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/gifts" element={<GiftsPage />} />
         <Route path="/anniversaries" element={<Anniversaries />} />
--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,6 +3,7 @@ import "./Footer.scss";
 const Footer = ({ selectedItems }) => {
   let sum = 0;
   let array = selectedItems.map((item) => item.price);
+  //   console.log(selectedItems);
 
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
