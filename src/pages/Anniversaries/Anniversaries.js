4c67c4149b4a7d60fbf636cf4d3caa9170d0efc5
Calvin <calvinng1993@gmail.com>
2024-01-26 09:18:14 +0000

    Add features to gift page

--- src/pages/Anniversaries/Anniversaries.js
+++ src/pages/Anniversaries/Anniversaries.js
@@ -1,10 +1,13 @@
 import "./Anniversaries.scss";
 import backArrow from "../../assets/icons/arrow-left.svg";
+import { Link } from "react-router-dom";
 const Anniversaries = () => {
   return (
     <>
       <header className="gifts">
-        <img src={backArrow} alt="" className="gifts__img" />
+        <Link to="/gifts">
+          <img src={backArrow} alt="" className="gifts__img" />
+        </Link>
         <h1 className="gifts__title">Anniversaries</h1>
         <h3 className="gifts__description">
           Get something special for a special someone
@@ -16,6 +19,17 @@ const Anniversaries = () => {
           Manage gift addresses
         </button>
       </section>
+      <main className="anniversary">
+        <article>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <h3 className="shop__title">Whole Foods Market</h3>
+              <p className="shop__description">5-15 min &#183; Free delivery</p>
+            </div>
+          </div>
+        </article>
+      </main>
     </>
   );
 };
